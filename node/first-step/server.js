315fd46cfce7158aabe6cb8bfc27570b0379dfc8
tiago-santos-bandeiranteslog <tiago.santos@bandeiranteslog.com.br>
2018-11-14 17:48:32 -0200

    nodejs

--- node/first-step/server.js
+++ node/first-step/server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const app = express();
-app.listen(3000, () => {
-    console.log('Server started!');
-});
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
